ca660fa252a156d2a6a275e4efeec94c7cac1dec
FloresRG <deux.flores@gmail.com>
2026-04-08 12:21:35 -0400

    fix:no existia el sobre nosotros

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,6 @@ export const Navbar = () => {
     { name: "Servicios", href: "/#services" },
     { name: "Portafolio", href: "/#portfolio" },
     { name: "Proyectos", href: "/proyectos" },
-    { name: "Sobre Nosotros", href: "/#about" },
     // contacto se abre en WhatsApp por ahora
     { name: "Contacto", href: "https://wa.me/59169867332?text=Hola%20MiraCode!%20Me%20interesa%20conocer%20m%C3%A1s%20sobre%20sus%20servicios%20%F0%9F%91%8B" },
     { name: "Precios", href: "/precios" },
